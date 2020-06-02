aa9909237cb99eee89668599ab65676391e67452
lauren Yeadon <laurenyeadon@laurens-MacBook-Air-2.local>
2020-06-02 13:37:01 -0300

    add first GET route

--- server.js
+++ server.js
@@ -2,6 +2,19 @@
 const express = require ('express'); //gives access to express library
 const app= express(); //creates instance of express constructor "app"
 
+
+const mockUserData=[
+{name:'Mark'},
+{name:'Jill'}
+]
+app.get('/users', function(req,res){
+ 	res.json({
+ 	 	success: true,
+ 	 	message: 'successfully got users. Nice!',
+ 	 	users: mockUserData
+ 	})
+})
+
 app.listen(8000, function(){
 console.log("server is running")
 })
